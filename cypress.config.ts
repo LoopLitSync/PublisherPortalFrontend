c1dfe84a0d7ddaf6505049534a7fc1fc534d6838
Robin <robintveraen@hotmail.com>
2025-03-07 08:42:41 +0100

    CHORE: setup e2e in cypress

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { defineConfig } from "cypress";
 
 export default defineConfig({
@@ -5,6 +6,12 @@ export default defineConfig({
     devServer: {
       framework: "react",
       bundler: "vite",
+    },
+  },
+
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
     },
   },
 });

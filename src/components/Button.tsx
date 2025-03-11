30635e34738eb86be7c5650dbed59aea0efd5848
Stine Szymanek <stine.szymanek@gmail.com>
2025-03-11 10:49:16 +0100

    FEAT: add profile page

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,16 +1,13 @@
-import React from "react";
-// npm i styled-components
-
-
 interface ButtonProps {
-  label: string;
-  onClick: () => void;
+  text: string;
 }
 
-
-
-const Button: React.FC<ButtonProps> = ({ label, onClick }) => {
-  return <button onClick={onClick}>{label}</button>;
-};
+const Button: React.FC<ButtonProps> = ({ text }) => {
+  return (
+    <button className="bg-[#8075FF] text-white px-4 py-2 rounded-lg">
+      {text}
+    </button>
+  )
+}
 
 export default Button;

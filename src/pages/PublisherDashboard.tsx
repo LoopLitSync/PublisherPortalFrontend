c91d32a5697c2332e2d88a1ca32a3664c0987a75
Zhiyi Wang <zhiyi.wang00@gmail.com>
2025-03-11 09:36:19 +0100

    Litsync 39 create navbar (#6)
    * FEAT: install Tailwind for styling
    
    * FEAT: add routing and basic navbar
    
    * FEAT: add dropdown menu and effects to navbar

---
+++ src/pages/PublisherDashboard.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const PublisherDashboard: React.FC = () => {
+    return (
+        <div>
+            <h1>Publisher Dashboard</h1>
+        </div>
+    );
+};
+
+export default PublisherDashboard;

8ee8128ee7b3a8ed7c00d4d3d13627bda1e0b4e2
Robin <robintveraen@hotmail.com>
2025-03-11 11:10:43 +0100

    STYLE: make table borders black

--- src/components/BookTable.tsx
+++ src/components/BookTable.tsx
@@ -11,26 +11,26 @@ const BookTable = () => {
 
   return (
     <div className="p-6">
-      <table className="w-full border border-gray-400 bg-white shadow-lg">
+      <table className="w-full border border-black bg-white shadow-lg">
         <thead>
-          <tr className="bg-[#8075FF] text-white text-left border-b border-gray-400">
-            <th className="p-3 border-r border-gray-300">ISBN</th>
-            <th className="p-3 border-r border-gray-300">Title</th>
-            <th className="p-3 border-r border-gray-300">Description</th>
-            <th className="p-3 border-r border-gray-300">Submitted Date</th>
-            <th className="p-3 border-r border-gray-300">Last Modified</th>
+          <tr className="bg-[#8075FF] text-white text-left border-b border-black">
+            <th className="p-3 border-r border-black">ISBN</th>
+            <th className="p-3 border-r border-black">Title</th>
+            <th className="p-3 border-r border-black">Description</th>
+            <th className="p-3 border-r border-black">Submitted Date</th>
+            <th className="p-3 border-r border-black">Last Modified</th>
             <th className="p-3">Status</th>
           </tr>
         </thead>
         <tbody>
           {books.length > 0 ? (
             books.map((book, index) => (
-              <tr key={index} className="hover:bg-gray-100 border-b border-gray-300">
-                <td className="p-3 border-r border-gray-300">{book.isbn}</td>
-                <td className="p-3 border-r border-gray-300">{book.title}</td>
-                <td className="p-3 border-r border-gray-300">{book.description}</td>
-                <td className="p-3 border-r border-gray-300">{book.publicationDate}</td>
-                <td className="p-3 border-r border-gray-300">-</td>
+              <tr key={index} className="hover:bg-gray-100 border-b border-black">
+                <td className="p-3 border-r border-black">{book.isbn}</td>
+                <td className="p-3 border-r border-black">{book.title}</td>
+                <td className="p-3 border-r border-black">{book.description}</td>
+                <td className="p-3 border-r border-black">{book.publicationDate}</td>
+                <td className="p-3 border-r border-black">-</td>
                 <td className="p-3">-</td>
               </tr>
             ))

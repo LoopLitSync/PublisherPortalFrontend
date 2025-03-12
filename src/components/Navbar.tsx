6cbafd0093280ee0a21361828d1ca9f5b5d11481
Zhiyi Wang <zhiyi.wang00@gmail.com>
2025-03-12 08:57:07 +0100

    FEAT: add keycloak log out logic

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useRef, useState } from 'react';
 import { NavLink } from 'react-router-dom';
-// import { useKeycloak } from '@react-keycloak/web';
+import keycloak from '../keycloak';
 
 const Navbar: React.FC = () => {
     const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -9,9 +9,7 @@ const Navbar: React.FC = () => {
     const profileMenuRef = useRef<HTMLDivElement>(null);
     const mobileMenuRef = useRef<HTMLDivElement>(null);
 
-    // const { keycloak } = useKeycloak();
-
-    // const logout = () => { keycloak?.logout(); };
+    const handleLogout = () => { keycloak.logout({redirectUri: window.location.origin }); };
 
     useEffect(() => {
         const handleClickOutside = (event: MouseEvent) => {
@@ -122,10 +120,8 @@ const Navbar: React.FC = () => {
                             <button
                                 className="flex items-center px-4 py-2 w-full text-red-600 hover:bg-gray-200" style={{ cursor: "pointer" }}
                                 onClick={() => {
-                                    // logout();
+                                    handleLogout();
                                     setIsProfileMenuOpen(false);
-                                    alert("Logging out...");
-                                    location.href = "/";
                                 }}>
                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="size-5 mr-2">
                                     <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 9V5.25A2.25 2.25 0 0 1 10.5 3h6a2.25 2.25 0 0 1 2.25 2.25v13.5A2.25 2.25 0 0 1 16.5 21h-6a2.25 2.25 0 0 1-2.25-2.25V15m-3 0-3-3m0 0 3-3m-3 3H15" />

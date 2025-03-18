a7a6d2c16a24256ef21f40f27eae870031678a55
Stine Szymanek <stine.szymanek@gmail.com>
2025-03-18 14:48:50 +0100

    FIX: show genres with space instead of underscore

--- src/pages/BookDetails.tsx
+++ src/pages/BookDetails.tsx
@@ -152,77 +152,79 @@ function BookDetails() {
           </thead>
           <tbody>
             {bookVersions.length > 0 ? (
-              bookVersions.slice().reverse().map((book, index) => (
-                <React.Fragment key={index}>
-                  {/* Main Row */}
-                  <tr
-                    className="hover:bg-gray-100 border-b border-black cursor-pointer"
-                    onClick={() => toggleRow(index)}
-                  >
-                    <td className="p-3 border-r border-black">{book.isbn}</td>
-                    <td className="p-3 border-r border-black">{book.title}</td>
-                    <td className="p-3 border-r border-black">{book.id.version}</td>
-                    <td className="p-3 border-r border-black">{formatDate(book.updatedDate)}</td>
-                    <td className="p-3 border-r border-black">{book.validationStatus}</td>
-                    <td className="p-3 text-center border-black">
-                      <span
-                        className={`transition-transform duration-300 inline-block ${expandedRow === index ? "rotate-180" : ""
-                          }`}
-                      >
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-7">
-                          <path fillRule="evenodd" d="M12.53 16.28a.75.75 0 0 1-1.06 0l-7.5-7.5a.75.75 0 0 1 1.06-1.06L12 14.69l6.97-6.97a.75.75 0 1 1 1.06 1.06l-7.5 7.5Z" clipRule="evenodd" />
-                        </svg>
-                      </span>
-                    </td>
-
-                  </tr>
-
-                  {/* Expanded Row */}
-                  {expandedRow === index && (
-                    <tr className="bg-gray-50 border-b border-black">
-                      <td colSpan={5} className="p-4">
-                        <div className="grid grid-cols-1 md:grid-cols-[20%_80%] gap-4 relative">
-                          {/* Cover Image */}
-                          {book.coverImage ? (
-                            <img className="rounded-lg w-56 h-80 object-cover" src={book.coverImage} alt={book.title} />
-                          ) : (
-                            <div className="w-56 h-80 flex items-center justify-center rounded-lg border border-gray-400 bg-gradient-to-br from-white to-[#8075FF] text-gray-700">
-                              <BookIcon className="size-12 text-[#8075FF]" />
-                            </div>
-                          )}
-
-                          {/* Additional Details */}
-                          <div className="flex flex-col gap-2">
-                            <p><strong>Description:</strong> {book.description}</p>
-                            <p><strong>Language:</strong> {book.language}</p>
-                            <p><strong>Publication Date:</strong> {book.publicationDate}</p>
-                            <p><strong>Genres:</strong> {book.genres.map(g => g.type).join(", ")}</p>
-                            <p><strong>Authors:</strong></p>
-                            <ul className="list-disc ml-6">
-                              {book.authors.map(author => (
-                                <li key="">{author.firstName} {author.lastName} ({author.year})</li>
-                              ))}
-                            </ul>
-                          </div>
-                          <div className="absolute bottom-0 right-0">
-                            {!book.activeVersion ? (
-                              <Button text="Rollback"
-                                onClick={() => {
-                                  const confirmRollback = window.confirm("Are you sure you want to rollback to this previous version?");
-                                  if (confirmRollback) {
-                                    onRollback(book.id.bookId, book.id.version);
-                                  }
-                                }}></Button>
-                            ) : (
-                              <p>Current version</p>
-                            )}
-                          </div>
-                        </div>
+              bookVersions
+                .sort((a, b) => b.id.version - a.id.version)
+                .map((book, index) => (
+                  <React.Fragment key={index}>
+                    {/* Main Row */}
+                    <tr
+                      className="hover:bg-gray-100 border-b border-black cursor-pointer"
+                      onClick={() => toggleRow(index)}
+                    >
+                      <td className="p-3 border-r border-black">{book.isbn}</td>
+                      <td className="p-3 border-r border-black">{book.title}</td>
+                      <td className="p-3 border-r border-black">{book.id.version}</td>
+                      <td className="p-3 border-r border-black">{formatDate(book.updatedDate)}</td>
+                      <td className="p-3 border-r border-black">{book.validationStatus}</td>
+                      <td className="p-3 text-center border-black">
+                        <span
+                          className={`transition-transform duration-300 inline-block ${expandedRow === index ? "rotate-180" : ""
+                            }`}
+                        >
+                          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-7">
+                            <path fillRule="evenodd" d="M12.53 16.28a.75.75 0 0 1-1.06 0l-7.5-7.5a.75.75 0 0 1 1.06-1.06L12 14.69l6.97-6.97a.75.75 0 1 1 1.06 1.06l-7.5 7.5Z" clipRule="evenodd" />
+                          </svg>
+                        </span>
                       </td>
+
                     </tr>
-                  )}
-                </React.Fragment>
-              ))
+
+                    {/* Expanded Row */}
+                    {expandedRow === index && (
+                      <tr className="bg-gray-50 border-b border-black">
+                        <td colSpan={5} className="p-4">
+                          <div className="grid grid-cols-1 md:grid-cols-[20%_80%] gap-4 relative">
+                            {/* Cover Image */}
+                            {book.coverImage ? (
+                              <img className="rounded-lg w-56 h-80 object-cover" src={book.coverImage} alt={book.title} />
+                            ) : (
+                              <div className="w-56 h-80 flex items-center justify-center rounded-lg border border-gray-400 bg-gradient-to-br from-white to-[#8075FF] text-gray-700">
+                                <BookIcon className="size-12 text-[#8075FF]" />
+                              </div>
+                            )}
+
+                            {/* Additional Details */}
+                            <div className="flex flex-col gap-2">
+                              <p><strong>Description:</strong> {book.description}</p>
+                              <p><strong>Language:</strong> {book.language}</p>
+                              <p><strong>Publication Date:</strong> {book.publicationDate}</p>
+                              <p><strong>Genres:</strong> {book.genres.map(g => g.type).join(", ")}</p>
+                              <p><strong>Authors:</strong></p>
+                              <ul className="list-disc ml-6">
+                                {book.authors.map(author => (
+                                  <li key="">{author.firstName} {author.lastName} ({author.year})</li>
+                                ))}
+                              </ul>
+                            </div>
+                            <div className="absolute bottom-0 right-0">
+                              {!book.activeVersion ? (
+                                <Button text="Rollback"
+                                  onClick={() => {
+                                    const confirmRollback = window.confirm("Are you sure you want to rollback to this previous version?");
+                                    if (confirmRollback) {
+                                      onRollback(book.id.bookId, book.id.version);
+                                    }
+                                  }}></Button>
+                              ) : (
+                                <p>Current version</p>
+                              )}
+                            </div>
+                          </div>
+                        </td>
+                      </tr>
+                    )}
+                  </React.Fragment>
+                ))
             ) : (
               <tr>
                 <td colSpan={5} className="text-center p-4 border-t border-gray-300">

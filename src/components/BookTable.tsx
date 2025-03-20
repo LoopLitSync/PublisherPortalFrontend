beb697516ba62750da93faa96412481b34780f81
Robin <robintveraen@hotmail.com>
2025-03-20 09:35:41 +0100

    FEAT: add author and description length cutoff

--- src/components/BookTable.tsx
+++ src/components/BookTable.tsx
@@ -4,11 +4,17 @@ import { Book } from "../models/Book";
 import { useNavigate } from "react-router-dom";
 import { formatDate } from "../utils/date.ts";
 import { useAuth } from "../AuthContext.tsx";
+import Button from "./Button.tsx";
 
 const BookTable = () => {
   const [books, setBooks] = useState<Book[]>([]);
   const navigate = useNavigate();
   const { publisher } = useAuth();
+
+ const formatDateWithoutTime = (dateString: string) => {
+    const date = new Date(dateString);
+    return date.toLocaleDateString([], { year: "numeric", month: "2-digit", day: "2-digit" }); 
+  };
   
   useEffect(() => {
     if (publisher && publisher.id !== undefined) {
@@ -27,8 +33,10 @@ const BookTable = () => {
           <tr className="bg-[#8075FF] text-white text-left border-b border-black">
             <th className="p-3 border-r border-black">ISBN</th>
             <th className="p-3 border-r border-black">Title</th>
+            <th className="p-3 border-r border-black">Author</th>
+            <th className="p-3 border-r border-black">Publication Date</th>
             <th className="p-3 border-r border-black">Description</th>
-            <th className="p-3 border-r border-black">Submitted Date</th>
+            <th className="p-3 border-r border-black">Submitted</th>
             <th className="p-3 border-r border-black">Last Modified</th>
             <th className="p-3">Status</th>
           </tr>
@@ -41,9 +49,15 @@ const BookTable = () => {
               onClick={() => navigate(`/book/${book.id}`)}>
                 <td className="p-3 border-r border-black">{book.isbn}</td>
                 <td className="p-3 border-r border-black">{book.title}</td>
-                <td className="p-3 border-r border-black">{book.description}</td>
+                <td className="p-3 border-r border-black">
+                  {book.authors.map((author) => `${author.firstName} ${author.lastName}`).join(", ")}
+                </td>
+                <td className="p-3 border-r border-black">{book.publicationDate}</td>
+                <td className="p-3 border-r border-black">
+                  {book.description.length > 100 ? `${book.description.slice(0, 20)}...` : book.description}
+                </td>
                 <td className="p-3 border-r border-black">{formatDate(book.submissionDate)}</td>
-                <td className="p-3 border-r border-black">{formatDate(book.updatedDate)}</td>
+                <td className="p-3 border-r border-black">{formatDateWithoutTime(book.updatedDate)}</td>
                 <td className="p-3 border-r border-black">{book.validationStatus}</td>
               </tr>
             ))
@@ -56,6 +70,9 @@ const BookTable = () => {
           )}
         </tbody>
       </table>
+      <div className="flex justify-end mt-5">
+      <Button onClick={() => navigate("/book-submission")}>Submit New Book</Button>
+      </div>
     </div>
   );
 };
